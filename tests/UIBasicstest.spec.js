5b3bf85e7e9a4557ac8549b93605810c1b9eb253
VinodKumbar <vnkumbar@gmail.com>
2022-12-11 21:59:47 +0530

    UI Controls and Child Windows Handling

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -21,7 +21,7 @@ test('Browser Context Playwright Test', async ({browser})=> {
  
 });
 
-test.only('Page Playwright Test', async ({page})=> {
+test('Page Playwright Test', async ({page})=> {
     const userName = page.locator("#username");
     const passWord = page.locator("[type='password']");
     const errorText = page.locator("[style*='block']");
@@ -43,9 +43,51 @@ test.only('Page Playwright Test', async ({page})=> {
       signInBtn.click(),
     ]);
     
-    // console.log(await cardTitles.first().textContent());  // first element
+    // console.log(await cardTitles.first().textContent());  // first element or .last() for last element
     // console.log(await cardTitles.nth(1).textContent());   // second element
 
     const allTitles = await cardTitles.allTextContents(); // all elements - allTextContents method is not part of Auto Wait
     console.log(allTitles);
-  });
+});
+
+
+test("UI Controls", async ({page})=>{
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    const userName = page.locator("#username");
+    const signInBtn = page.locator("#signInBtn");
+    const dropdown = page.locator("select.form-control"); 
+    const documentLink = page.locator("[href*='documents-request']")
+    await dropdown.selectOption("consult");
+    await page.locator(".radiotextsty").nth(1).click();
+    await page.locator("#okayBtn").click();
+    expect(page.locator(".radiotextsty").nth(1)).toBeChecked();
+    console.log(await page.locator(".radiotextsty").nth(1).isChecked());
+    // await page.pause();            // it will open Playwright Inspector
+    await page.locator("#terms").click();
+    await expect(page.locator("#terms")).toBeChecked();
+    await page.locator("#terms").uncheck();
+    expect(await page.locator("#terms").isChecked()).toBeFalsy();
+    await expect(documentLink).toHaveAttribute("class", "blinkingText");
+});
+
+test.only("Child windows handling", async ({browser})=>{
+  const context = await browser.newContext(); // it will open fresh new instance - browser in incognito mode
+  const page = await context.newPage();
+  const userName = page.locator("#username");
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  const documentLink = page.locator("[href*='documents-request']")
+
+  const [newPage] = await Promise.all([
+    context.waitForEvent('page'),
+    documentLink.click(),
+    ])
+  const text = await newPage.locator(".red").textContent();
+  console.log(text);
+  const arrayText = text.split("@")
+  const domainName = arrayText[1].split(" ")[0]
+  console.log(domainName);
+  await userName.type(domainName);
+  await page.pause();
+  console.log( await userName.textContent());
+
+});
